0b119cd4fd8f0030e5d6af453252a9cf6025cbb8
andrewblond <andrewblond@yandex.com>
2013-07-07 13:33:20 +0400

    Added doc for `bemhtml-old` tech

--- techs/bemhtml-old.js
+++ techs/bemhtml-old.js
@@ -1,3 +1,30 @@
+/**
+ * bemhtml-old
+ * ===========
+ *
+ * Склеивает *bemhtml.xjst* и *bemhtml*-файлы по deps'ам, обрабатывает BEMXJST-транслятором,
+ * сохраняет (по умолчанию) в виде `?.bemhtml.js`. Использует компилятор, входящий в состав библиотеки
+ * `bem-core` (https://github.com/bem/bem-core). Поддерживает как новый js-совместимый, так и старый синтаксис.
+ * Транслирование из старого синтаксиса в новый осуществляется с помощью библиотеки
+ * `bemhtml-compat` (https://github.com/bem/bemhtml-compat).
+ * **Внимание:** считается, что файлы с суффиксом `*.xjst` могут быть написаны только в новом синтаксисе
+ * (старый синтаксис не поддерживается).
+ *
+ * **Опции**
+ *
+ * * *String* **target** — Результирующий таргет. По умолчанию — `?.bemhtml.js`.
+ * * *String* **filesTarget** — files-таргет, на основе которого получается список исходных файлов
+ *   (его предоставляет технология `files`). По умолчанию — `?.files`.
+ * * *String* **exportName** — Имя переменной-обработчика BEMHTML. По умолчанию — `'BEMHTML'`.
+ * * *Boolean* **devMode** — Development-режим. По умолчанию — `true`.
+ * * *Boolean* **cache** — Кеширование. По умолчанию — `true`.
+ *
+ * **Пример**
+ *
+ * ```javascript
+ * nodeConfig.addTech(require('enb-bemxjst/techs/bemhtml-old'));
+ * ```
+ */
 module.exports = require('./bemhtml').buildFlow()
     .name('bemhtml-old')
     .builder(function(sourceFiles) {
