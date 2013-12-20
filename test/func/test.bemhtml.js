e4adf719d04d6a89b63c0d5a67dd4237e6b735b5
andrewblond <andrewblond@yandex.com>
2013-12-20 16:12:22 +0400

    Tests refactoring

--- test/func/test.bemhtml.js
+++ test/func/test.bemhtml.js
@@ -1,28 +1,30 @@
-var fs = require('fs');
+var fs = require('fs'),
+    path = require('path'),
+    fixturesPath = path.join(__dirname, '..', 'fixtures', 'bemhtml'),
+    devBemhtmlPath = path.join(fixturesPath, 'page', 'page.dev.bemhtml.js'),
+    prodBemhtmlPath = path.join(fixturesPath, 'page', 'page.prod.bemhtml.js'),
+    view = require(path.join(fixturesPath, 'data', 'view.json')),
+    html = fs.readFileSync(path.join(fixturesPath, 'result', 'page.html'), 'utf8');
 
 require('chai').should();
 
 describe('functional', function() {
     describe('bemhtml', function() {
-        it('should builds simple page in dev mode', function() {
-            var bemhtml = require('../fixtures/bemhtml/page/page.dev.bemhtml').BEMHTML,
-                view    = require('../fixtures/bemhtml/data/view.json'),
-                html    = fs.readFileSync('./test/fixtures/bemhtml/result/page.html', 'utf8');
+        it('should build simple page in dev mode', function() {
+            var bemhtml = require(devBemhtmlPath).BEMHTML;
 
             bemhtml.apply(view).should.equal(html);
         });
 
-        it('should builds simple page in production mode', function() {
-            var bemhtml = require('../fixtures/bemhtml/page/page.prod.bemhtml').BEMHTML,
-                view    = require('../fixtures/bemhtml/data/view.json'),
-                html    = fs.readFileSync('./test/fixtures/bemhtml/result/page.html', 'utf8');
+        it('should build simple page in production mode', function() {
+            var bemhtml = require(prodBemhtmlPath).BEMHTML;
 
             bemhtml.apply(view).should.equal(html);
         });
 
         it('should build different code by mode', function() {
-            var devStat = fs.statSync('./test/fixtures/bemhtml/page/page.dev.bemhtml.js'),
-                prodStat = fs.statSync('./test/fixtures/bemhtml/page/page.prod.bemhtml.js');
+            var devStat = fs.statSync(devBemhtmlPath),
+                prodStat = fs.statSync(prodBemhtmlPath);
 
             devStat.size.should.be.above(prodStat.size);
         });
--- test/func/test.bemtree-old.js
+++ test/func/test.bemtree-old.js
@@ -1,32 +1,36 @@
-var fs = require('fs');
+var fs = require('fs'),
+    path = require('path'),
+    fixturesPath = path.join(__dirname, '..', 'fixtures', 'bemtree-old'),
+    devBemtreePath = path.join(fixturesPath, 'page', 'page.dev.bemtree.js'),
+    prodBemtreePath = path.join(fixturesPath, 'page', 'page.prod.bemtree.js'),
+    data = require(path.join(fixturesPath, 'data', 'data.json')),
+    view = require(path.join(fixturesPath, 'result', 'view.json'));
 
 require('chai').should();
 
 describe('functional', function() {
     describe('bemtree-old', function() {
-        it('should builds simple view of page in dev mode', function() {
-            var bemtree = require('../fixtures/bemtree-old/page/page.dev.bemtree').BEMTREE,
-                data    = require('../fixtures/bemtree-old/data/data.json'),
-                view    = require('../fixtures/bemtree-old/result/view.json');
+        it('should build simple view of page in dev mode', function(done) {
+            var bemtree = require(devBemtreePath).BEMTREE;
 
             bemtree.apply(data).then(function(res) {
-                JSON.stringify(res).should.equal(JSON.stringify(view));
-            }).done();
+                res.should.eql(view);
+                done();
+            });
         });
 
-        it('should builds simple view of page in production mode', function() {
-            var bemtree = require('../fixtures/bemtree-old/page/page.prod.bemtree').BEMTREE,
-                data    = require('../fixtures/bemtree-old/data/data.json'),
-                view    = require('../fixtures/bemtree-old/result/view.json');
+        it('should build simple view of page in production mode', function(done) {
+            var bemtree = require(prodBemtreePath).BEMTREE;
 
             bemtree.apply(data).then(function(res) {
-                JSON.stringify(res).should.equal(JSON.stringify(view));
-            }).done();
+                res.should.eql(view);
+                done();
+            });
         });
 
         it('should build different code by mode', function() {
-            var devStat = fs.statSync('./test/fixtures/bemtree-old/page/page.dev.bemtree.js'),
-                prodStat = fs.statSync('./test/fixtures/bemtree-old/page/page.prod.bemtree.js');
+            var devStat = fs.statSync(devBemtreePath),
+                prodStat = fs.statSync(prodBemtreePath);
 
             devStat.size.should.be.above(prodStat.size);
         });
