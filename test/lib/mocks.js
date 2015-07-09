cc28b1752a6f3fe4c7708620ddb66466a56a9151
Anton Usmansky <cody0@yandex-team.ru>
2015-07-09 11:33:45 +0300

    Using shared JobQueue instead of creating processes via sibling

--- test/lib/mocks.js
+++ test/lib/mocks.js
@@ -2,7 +2,7 @@ var mock = require('mock-require'),
     ometajsPath = require.resolve('bemhtml-compat/node_modules/ometajs'),
     bemhtmlPath  = require.resolve('bemhtml-compat/lib/ometa/bemhtml.ometajs');
 
-mock(require.resolve('sibling'), require('./mock-sibling'));
-
 mock(require.resolve(ometajsPath), require(ometajsPath));
 mock(require.resolve(bemhtmlPath), require(bemhtmlPath));
+
+require('./job-queue-stub');
