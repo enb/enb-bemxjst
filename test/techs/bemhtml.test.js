18019d591cb90c9cb304b2fe70ac7291fe037927
blond <andrewblond@yandex.com>
2016-02-26 14:11:25 +0300

    Moved JobQueueStub to bemhtml tests

--- test/techs/bemhtml.test.js
+++ test/techs/bemhtml.test.js
@@ -8,6 +8,15 @@ var fs = require('fs'),
     bundlePath = path.resolve('lib/bundle.js');
 
 describe('bemhtml', function () {
+    before(function () {
+        var JobQueueStub = require('mock-enb/lib/job-queue-stub');
+
+        // Использование mock-fs не позволяет подключить bemxjst-processor в рантайме
+        // https://github.com/tschaub/mock-fs/issues/12
+        // поэтому подключаем его перед инициализацией mock-fs
+        JobQueueStub.prototype.processor = require('../../lib/bemhtml-processor');
+    });
+
     afterEach(function () {
         mock.restore();
     });
