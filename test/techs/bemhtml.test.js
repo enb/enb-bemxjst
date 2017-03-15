9929d33ee87998b4be68c608144fa6bd92ca564d
miripiruni <miripiruni@gmail.com>
2017-03-15 14:55:29 +0300

    Test: comments about mock-fs

--- test/techs/bemhtml.test.js
+++ test/techs/bemhtml.test.js
@@ -301,6 +301,7 @@ function build(templates, options) {
         });
     }
 
+    // mock for bemhtml.generate()
     var bemhtmlPath = './node_modules/bem-xjst/lib/bemhtml/bundle.js'
     scheme[bemhtmlPath] = require('fs').readFileSync(bemhtmlPath, 'utf8');
 
--- test/techs/bemtree.test.js
+++ test/techs/bemtree.test.js
@@ -213,6 +213,7 @@ function build(templates, options) {
         });
     }
 
+    // mock for bemhtml.generate()
     var bemtreePath = './node_modules/bem-xjst/lib/bemtree/bundle.js'
     scheme[bemtreePath] = require('fs').readFileSync(bemtreePath, 'utf8');
 
